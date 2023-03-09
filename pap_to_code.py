14272b65d5b4b3011ba286c9837366e976d39ab9
ceddy2001 <cedricklingler@gmail.com>
2023-03-09 15:24:35 +0100

    Update pap_to_code.py
    hallo

--- pap_to_code.py
+++ pap_to_code.py
@@ -9,4 +9,14 @@ else:
     elif license_request == 1:
         
 
+print("Hast du Alkohol GEtrunken? (y/n)")
+alkohol = input()
 
+if alkohol == y:
+
+        i + 1
+
+
+
+elif alkohol == n: 
+    print("Nach Hause fahren")
