195d02c0984221b538f7a1ce4393c2cb4f7932c4
lulurnk <126180314+lulurnk@users.noreply.github.com>
2023-03-09 15:27:24 +0100

    Update pap_to_code.py

--- pap_to_code.py
+++ pap_to_code.py
@@ -6,15 +6,12 @@ else:
     license_request = int(input("Do you have a license?:\n(1) Yes\(2) No"))
     if license_request == 2:
         not_allowed = True
-    elif license_request == 1:
-        
+    elif license_request == 1:    
+        alcohol = input("Hast du Alkohol GEtrunken? (y/n)")
 
-print("Hast du Alkohol GEtrunken? (y/n)")
-alkohol = input()
 
 if alkohol == y:
 
-        i + 1
 
 
 
