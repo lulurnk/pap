6a633dd433e11fa91de17ecc190a39bfee691b55
ceddy2001 <cedricklingler@gmail.com>
2023-03-09 15:33:50 +0100

    function

--- pap_to_code.py
+++ pap_to_code.py
@@ -17,3 +17,16 @@ if alkohol == y:
 
 elif alkohol == n: 
     print("Nach Hause fahren")
+
+def alcohol_test():
+    print("Das ist der Alkoholtest")
+    
+    test1 = int(input("Was ist 2 + 2?"))
+    if test1 == 4: 
+        print("Test 1/3 bestanden")
+    test2 = int(input("Was ist 4 + 4?"))
+    if test2 == 8: 
+        print("Test 2/3 bestanden")
+    test3 = int(input("Was ist 10 - 5?"))
+    if test3 == 5: 
+        print("Test 3/3 bestanden")
