d7b1964406f004468bd9105b4cadbaba015a8b50
lulurnk <126180314+lulurnk@users.noreply.github.com>
2023-03-09 15:02:20 +0100

    added first two questions

---
+++ pap_to_code.py
@@ -0,0 +1,11 @@
+age = int(input("Wie alt bist du?: "))
+
+if not age >= 18:
+    not_allowed = True
+else:
+    license_request = int(input("Do you have a license?:\n(1) Yes\(2) No"))
+    if license_request == 2:
+        not_allowed = True
+    elif license_request == 1:
+
+
