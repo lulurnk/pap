5af5be6178f7fe4051c6e70633487aba07bb28e4
lulurnk <126180314+lulurnk@users.noreply.github.com>
2023-03-09 15:42:34 +0100

    final 2.0

--- pap_to_code.py
+++ pap_to_code.py
@@ -3,14 +3,14 @@ not_allowed = False
 def alcohol_test():
     print("Das ist der Alkoholtest")
     
-    test1 = int(input("Was ist 2 + 2?"))
+    test1 = int(input("Was ist 2 + 2?\n"))
     if test1 == 4: 
         print("Test 1/3 bestanden")
-        test2 = int(input("Was ist 4 + 4?"))
+        test2 = int(input("Was ist 4 + 4?\n"))
         test = False
         if test2 == 8: 
             print("Test 2/3 bestanden")
-            test3 = int(input("Was ist 10 - 5?"))
+            test3 = int(input("Was ist 10 - 5?\n"))
             test = False
             if test3 == 5: 
                 print("Test 3/3 bestanden")
@@ -23,16 +23,16 @@ def alcohol_test():
         test = True
     return test
 
-age = int(input("Wie alt bist du?: "))
+age = int(input("Wie alt bist du?:\n "))
 
 if not age >= 18:
     not_allowed = True
 else:
-    license_request = int(input("Hast du einen Führerschein?:\n(1) Ja\(2) Nein"))
+    license_request = int(input("Hast du einen Führerschein?:\n(1) Ja\(2) Nein\n"))
     if license_request == 2:
         not_allowed = True
     elif license_request == 1:    
-        alcohol = input("Hast du Alkohol GEtrunken? (y/n)")
+        alcohol = input("Hast du Alkohol GEtrunken? (y/n)\n")
         if alcohol == 'y':
             test = alcohol_test()
             if test == True:
